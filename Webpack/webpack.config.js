e858a70f9fa98ff7878d4453699b925b064c8a0f
chienvu94 <chienvu@beeketing.net>
2020-01-18 21:53:49 +0700

    10. HTML webpack plugin

--- Webpack/webpack.config.js
+++ Webpack/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -37,7 +38,8 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: "styles.[contenthash].css"
         }),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin()
     ],
     mode: "development"
 }
