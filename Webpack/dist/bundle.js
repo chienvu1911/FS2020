948972f11628ad264162b8f1b16cae23baaf4baf
chienvu94 <chienvu@beeketing.net>
2020-01-18 16:47:34 +0700

    5.Use babel to transform modern js code

--- Webpack/dist/bundle.js
+++ Webpack/dist/bundle.js
@@ -129,7 +129,7 @@ eval("\n\nvar isOldIE = function isOldIE() {\n  var memo;\n  return function mem
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _avatar_jpg__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./avatar.jpg */ \"./src/avatar.jpg\");\n\r\n\r\nfunction addImage() {\r\n    const img = document.createElement(\"img\");\r\n    img.alt = 'avatar';\r\n    img.src = _avatar_jpg__WEBPACK_IMPORTED_MODULE_0__[\"default\"];\r\n\r\n    const body = document.querySelector(\"body\");\r\n    body.appendChild(img);\r\n}\r\n\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (addImage);\n\n//# sourceURL=webpack:///./src/add-image.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _avatar_jpg__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./avatar.jpg */ \"./src/avatar.jpg\");\n\n\nfunction addImage() {\n  var img = document.createElement(\"img\");\n  img.alt = 'avatar';\n  img.src = _avatar_jpg__WEBPACK_IMPORTED_MODULE_0__[\"default\"];\n  var body = document.querySelector(\"body\");\n  body.appendChild(img);\n}\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (addImage);\n\n//# sourceURL=webpack:///./src/add-image.js?");
 
 /***/ }),
 
@@ -153,7 +153,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ 
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _button_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./button.scss */ \"./src/button.scss\");\n/* harmony import */ var _button_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_button_scss__WEBPACK_IMPORTED_MODULE_0__);\n\r\n\r\nclass Button {\r\n    render() {\r\n        const button = document.createElement(\"button\");\r\n        button.innerHTML = \"Button\";\r\n        button.classList.add('my-button');\r\n        const body = document.querySelector(\"body\");\r\n        body.appendChild(button);\r\n    }\r\n}\r\n\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (Button);\n\n//# sourceURL=webpack:///./src/button.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _button_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./button.scss */ \"./src/button.scss\");\n/* harmony import */ var _button_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_button_scss__WEBPACK_IMPORTED_MODULE_0__);\nfunction _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError(\"Cannot call a class as a function\"); } }\n\nfunction _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if (\"value\" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }\n\nfunction _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }\n\n\n\nvar Button =\n/*#__PURE__*/\nfunction () {\n  function Button() {\n    _classCallCheck(this, Button);\n  }\n\n  _createClass(Button, [{\n    key: \"render\",\n    value: function render() {\n      var button = document.createElement(\"button\");\n      button.innerHTML = \"Button\";\n      button.classList.add('my-button');\n      var body = document.querySelector(\"body\");\n      body.appendChild(button);\n    }\n  }]);\n\n  return Button;\n}();\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (Button);\n\n//# sourceURL=webpack:///./src/button.js?");
 
 /***/ }),
 
@@ -176,7 +176,7 @@ eval("var api = __webpack_require__(/*! ../node_modules/style-loader/dist/runtim
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\nfunction hello() {\r\n    console.log('Hello');\r\n}\r\n\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (hello);\n\n//# sourceURL=webpack:///./src/hello.js?");
+eval("__webpack_require__.r(__webpack_exports__);\nfunction hello() {\n  console.log('Hello');\n}\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (hello);\n\n//# sourceURL=webpack:///./src/hello.js?");
 
 /***/ }),
 
@@ -188,7 +188,7 @@ eval("__webpack_require__.r(__webpack_exports__);\nfunction hello() {\r\n    con
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _hello__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./hello */ \"./src/hello.js\");\n/* harmony import */ var _add_image__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./add-image */ \"./src/add-image.js\");\n/* harmony import */ var _button__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./button */ \"./src/button.js\");\n\r\n\r\n\r\n\r\nObject(_hello__WEBPACK_IMPORTED_MODULE_0__[\"default\"])();\r\n// addImage();\r\nlet button = new _button__WEBPACK_IMPORTED_MODULE_2__[\"default\"]();\r\nbutton.render();\r\n\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _hello__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./hello */ \"./src/hello.js\");\n/* harmony import */ var _add_image__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./add-image */ \"./src/add-image.js\");\n/* harmony import */ var _button__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./button */ \"./src/button.js\");\n\n\n\nObject(_hello__WEBPACK_IMPORTED_MODULE_0__[\"default\"])(); // addImage();\n\nvar button = new _button__WEBPACK_IMPORTED_MODULE_2__[\"default\"]();\nbutton.render();\n\n//# sourceURL=webpack:///./src/index.js?");
 
 /***/ })
 
