2097711f46e32807b553f7f469306e6e983af163
chienvu94 <chienvu@beeketing.net>
2020-01-18 17:33:48 +0700

    7.Mini css extract plugin

--- Webpack/webpack.config.js
+++ Webpack/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -16,11 +17,11 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
             },
             {
                 test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
             },
             {   
                 test: /\.jsx?$/,   
@@ -31,7 +32,10 @@ module.exports = {
         ]
     },
     plugins: [
-        new TerserPlugin()
+        new TerserPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "styles.css"
+        })
     ],
     mode: "development"
 }
